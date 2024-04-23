56baea41a1fd977ecda98f5544d8d88f7e1f01a8
Harsora Krupa <harsorakrupa.nitsan@gmail.com>
2024-04-23 16:06:37 +0530

    [TODO] Replace Nitsan Keyword

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,4 +5,4 @@ defined('TYPO3_MODE') || die('Access denied.');
 $extKey = 'ns_feedback';
 
 // Adding fields to the tt_content table definition in TCA
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', '[NITSAN] Feedback');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'Feedback');
