7271a9571de7bfab0a4b41529a3c6360ddc54552
Harsora Krupa <harsorakrupa.nitsan@gmail.com>
2024-06-17 19:02:31 +0530

    [BUGFIX] Fix Multiple feedback issue

--- Classes/Controller/FeedbackController.php
+++ Classes/Controller/FeedbackController.php
@@ -6,6 +6,7 @@ use NITSAN\NsFeedback\Domain\Model\Feedbacks;
 use NITSAN\NsFeedback\Domain\Model\Report;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 
 class FeedbackController extends ActionController
 {
@@ -163,63 +164,62 @@ class FeedbackController extends ActionController
         $feedbacks->setPId($data['uid']);
         $feedbacks->setCId($result['cid']);
         $checkExistRecord = $this->reportRepository->findByPageId($data['uid']);
+
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $this->feedbacksRepository->setDefaultQuerySettings($querySettings);
+
         $checkExistFeedbackRecord = $this->feedbacksRepository->findByUserIp($_SERVER['REMOTE_ADDR']);
-        if ($checkExistRecord[0] && empty($checkExistFeedbackRecord[0])) {
+
+        if ($checkExistRecord[0]) {
             $report = $checkExistRecord[0];
-            switch ($result['buttonfor']) {
-                case '1':
-                    $getexistCount = ($checkExistRecord[0]->getFeedbackYesCount()) + 1;
-                    $report->setFeedbackYesCount($getexistCount);
-                    break;
-
-                case '2':
-                    $getexistCount = ($checkExistRecord[0]->getFeedbackNoCount()) + 1;
-                    $report->setFeedbackNoCount($getexistCount);
-                    break;
-
-                case '3':
-                    $getexistCount = ($checkExistRecord[0]->getFeedbackYesButCount()) + 1;
-                    $report->setFeedbackYesButCount($getexistCount);
-                    break;
-
-                case '4':
-                    $getexistCount = ($checkExistRecord[0]->getFeedbackNoButCount()) + 1;
-                    $report->setFeedbackNoButCount($getexistCount);
-                    break;
-                default:
-                    break;
-            }
-        } else {
-            switch ($result['buttonfor']) {
-                case '1':
-                    $report->setFeedbackYesCount(1);
-                    break;
-
-                case '2':
-                    $report->setFeedbackNoCount(1);
-                    break;
-
-                case '3':
-                    $report->setFeedbackYesButCount(1);
-                    break;
-
-                case '4':
-                    $report->setFeedbackNoButCount(1);
-                    break;
-                default:
-                    break;
-            }
         }
-        $report->addFeedback($feedbacks);
+
+        $report->setFeedbackYesCount(0);
+        $report->setFeedbackNoCount(0);
+        $report->setFeedbackYesButCount(0);
+        $report->setFeedbackNoButCount(0);
+
+        switch ($result['buttonfor']) {
+            case '1':
+                $report->setFeedbackYesCount(1);
+                break;
+
+            case '2':
+                $report->setFeedbackNoCount(1);
+                break;
+
+            case '3':
+                $report->setFeedbackYesButCount(1);
+                break;
+
+            case '4':
+                $report->setFeedbackNoButCount(1);
+                break;
+            default:
+                break;
+        }
+
         $report->setPageType($data['doktype']);
         $report->setPageId($data['uid']);
         $report->setPId($data['uid']);
         $report->setPageTitle($data['title']);
         $report->setSysLangId($this->sys_language_uid);
-        if ($checkExistRecord[0] && empty($checkExistFeedbackRecord[0])) {
+
+        if($checkExistFeedbackRecord[0]){
+            $checkExistFeedbackRecord[0]->setQuickfeedbacktype($result['qkbutton']);
+            $checkExistFeedbackRecord[0]->setComment("");
+            if ($result['buttonfor'] == 3 || $result['buttonfor'] == 4) {
+                $checkExistFeedbackRecord[0]->setComment($result['commentText']);
+            }
+            $this->feedbacksRepository->update($checkExistFeedbackRecord[0]);
+        }
+
+        if ($checkExistRecord[0]) {
             $this->reportRepository->update($report);
 
         } else {
+            $report->addFeedback($feedbacks);
             $this->reportRepository->add($report);
         }
         return 'OK';
