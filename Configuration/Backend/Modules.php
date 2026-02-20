2e845d9dab2bbfd0774db0f52e85f276e8e8e97e
Alfaz-Nitsan-Tech <alfaz.nitsan@gmail.com>
2026-02-20 13:15:35 +0530

    [TASK]Page tree not show

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,15 +1,21 @@
 <?php
 
-
-use NITSAN\NsFeedback\Controller\NsConstantEditorController;
 use NITSAN\NsFeedback\Controller\ReportController;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
+
+if ((GeneralUtility::makeInstance(Typo3Version::class))->getMajorVersion() <= 13) {
+    $navigationComponent = '@typo3/backend/page-tree/page-tree-element';
+} else {
+    $navigationComponent = '@typo3/backend/tree/page-tree-element';
+}
 
 return [
     'nitsan_module' => [
         'labels' => 'LLL:EXT:ns_feedback/Resources/Private/Language/BackendModule.xlf',
         'icon' => 'EXT:ns_feedback/Resources/Public/Icons/module-nsfeedback.svg',
         'iconIdentifier' => 'module-nsfeedback',
-        'navigationComponent' => '@typo3/backend/tree/page-tree-element',
+        'navigationComponent' => $navigationComponent,
         'position' => ['after' => 'web'],
     ],
     'nitsan_nsfeedbackmodule_report' => [
@@ -19,7 +25,7 @@ return [
         'path' => '/module/nitsan/NsFeedbackReport',
         'icon'   => 'EXT:ns_feedback/Resources/Public/Icons/plugin-feedback.svg',
         'labels' => 'LLL:EXT:ns_feedback/Resources/Private/Language/locallang_feedback_report.xlf',
-        'navigationComponent' => '@typo3/backend/tree/page-tree-element',
+        'navigationComponent' => $navigationComponent,
         'extensionName' => 'NsFeedback',
         'controllerActions' => [
             ReportController::class => [
