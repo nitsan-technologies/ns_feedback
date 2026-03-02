fa5c2275349f3575af2c5a478422a470320fdf83
Alfaz-Nitsan-Tech <alfaz.nitsan@gmail.com>
2026-03-02 12:35:01 +0530

    [TASK]Plugin Not show in typo3 v12

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -16,6 +16,3 @@ ExtensionUtility::registerPlugin(
     'ns_feedback-plugin-feedback',
     'plugins'
 );
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['nsfeedback_feedback'] = 'recursive,select_key,pages';
-$GLOBALS['TCA']['tx_nsfeedback_domain_model_feedbacks']['ctrl']['hideTable'] = 1;
-$GLOBALS['TCA']['tx_nsfeedback_domain_model_report']['ctrl']['hideTable'] = 1;
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,37 +4,20 @@ defined('TYPO3') || die('Access denied.');
 
 use NITSAN\NsFeedback\Controller\FeedbackController;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
-$versionNumber =  VersionNumberUtility::convertVersionStringToArray(VersionNumberUtility::getCurrentTypo3Version());
+ExtensionUtility::configurePlugin(
+    'NsFeedback',
+    'Feedback',
+    [
+        FeedbackController::class => 'new, quickFeedback, default',
+    ],
+    // non-cacheable actions
+    [
+        FeedbackController::class => 'new, quickFeedback, default',
+    ],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
 
-if ($versionNumber['version_main'] <= '12') {
-    // @extensionScannerIgnoreLine
-    ExtensionUtility::configurePlugin(
-        'NsFeedback',
-        'Feedback',
-        [
-            FeedbackController::class => 'new, quickFeedback, default',
-        ],
-        // non-cacheable actions
-        [
-            FeedbackController::class => 'new, quickFeedback, default',
-        ]
-    );
-} else{
-    ExtensionUtility::configurePlugin(
-        'NsFeedback',
-        'Feedback',
-        [
-            FeedbackController::class => 'new, quickFeedback, default',
-        ],
-        // non-cacheable actions
-        [
-            FeedbackController::class => 'new, quickFeedback, default',
-        ],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-}
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['Feedback'] = [
     'NITSAN\NsFeedback\ViewHelpers',
 ];
